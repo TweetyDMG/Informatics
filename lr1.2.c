bb21e360af1bf74eaf366b19611614120e9045fd
Artem Rogachev <144269255+TweetyDMG@users.noreply.github.com>
2024-02-26 18:54:46 +0300

    Update lr1.2.c

--- lr1.2.c
+++ lr1.2.c
@@ -20,6 +20,9 @@ int main() {
 
     printf("Число с учетом верных цифр: %.*f\n", valid_digits, number);
 
+    double absolute_error = pow(10, -valid_digits) / 2.0;
+    printf("Абсолютная погрешность: %.*f\n", valid_digits, absolute_error);
+
     system("pause");
     return 0;
 }
