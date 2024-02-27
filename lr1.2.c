35c1865eabbe8d994dd6f40847b172fa47cdc6a9
Artem Rogachev <144269255+TweetyDMG@users.noreply.github.com>
2024-02-27 19:09:30 +0300

    Update lr1.2.c

--- lr1.2.c
+++ lr1.2.c
@@ -9,15 +9,18 @@ int main() {
     double number;
     double precision;
 
-    printf("Введите число: ");
+    printf("\nВведите число: ");
     scanf("%lf", &number);
 
     printf("Введите точность погрешности в процентах: ");
     scanf("%lf", &precision);
+    system("clear");
 
+    printf("\nОкругление сомнительных цифр числа %.3lf, оставляя верные знаки. Определение абсолютной погрешности числа %.3lf\n", number, number);
     precision = precision / 100.0;
+    printf("Перевод процентного значения в число: %lf\n", precision);
     int valid_digits = -log10(precision);
-
+    printf("Количество верных цифр после запятой = log10(%lf) = %d\n", precision, valid_digits);
     printf("Число с учетом верных цифр: %.*f\n", valid_digits, number);
 
     double absolute_error = pow(10, -valid_digits) / 2.0;
@@ -26,4 +29,3 @@ int main() {
     system("pause");
     return 0;
 }
-
