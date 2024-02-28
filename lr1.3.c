b1df1a9bbcfde7db00f1b342d7d94bb654ea309a
Artem Rogachev <144269255+TweetyDMG@users.noreply.github.com>
2024-02-28 21:00:20 +0300

    Update lr1.3.c

--- lr1.3.c
+++ lr1.3.c
@@ -7,27 +7,15 @@
 int main()
 {
     setlocale(LC_CTYPE, "rus");
-
-    //есть число 0.5178. Найти предельные относительную и абсолютную погрешности. Все цифры верные
     double originalNumber, roundedNumber;
     int decimalPlaces;
-
-    // Ввод вещественного числа от пользователя
     printf("Введите вещественное число: ");
     scanf("%lf", &originalNumber);
-
-    // Ввод количества знаков после запятой для округления
     printf("До скольки знаков после запятой округлить число: ");
     scanf("%d", &decimalPlaces);
-
-    // Округление введенного числа
     roundedNumber = round(originalNumber * pow(10, decimalPlaces)) / pow(10, decimalPlaces);
-
-    // Расчет абсолютной и относительной погрешностей
     double absoluteError = fabs(originalNumber - roundedNumber);
-    double relativeError = (absoluteError / fabs(originalNumber)) * 100;
-
-    // Вывод результатов
+    double relativeError = (absoluteError / fabs(originalNumber)) * 100
     printf("Введенное число: %.7f\n", originalNumber);
     printf("Округленное число: %.7f\n", roundedNumber);
     printf("Абсолютная погрешность: %.7f\n", absoluteError);
