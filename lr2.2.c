b9d19c8f76224df976dfae811512f01775d08e8b
Artem Rogachev <144269255+TweetyDMG@users.noreply.github.com>
2024-04-06 19:51:42 +0300

    Update lr2.2.c

--- lr2.2.c
+++ lr2.2.c
@@ -9,19 +9,28 @@ double series(double x, double epsilon);
 
 int main(void)
 {
-	
-	double xn = 1;
-	double xk = 3;
+
+	double xn = 1.;
+	double xk = 3.;
 	double dx = 0.1;
 
-	printf("___________________________________________________________\n");
-	printf("|    x    |    eps1    |    f1    |    eps2    |    f2    |\n");
-	printf("|         |    0.1     |          |  0.000001  |          |\n");
+	printf("_______________________________________________\n");
+	printf("|   x  |  eps1 |   f1   |    eps2    |   f2   |\n");
+	printf("|      |  0.1  |        |  0.000001  |        |\n");
+	printf("|------|-------|--------|------------|--------|\n");
 	for (double x = xn; x <= xk; x += dx) {
 		double result1 = series(x, 0.1);
 		double result2 = series(x, 0.000001);
-		printf("");
+		// Проверка на NaN
+		if (isnan(result1) || result1 <= 0) {
+			result1 = NAN;
+		}
+		if (isnan(result2) || result2 <= 0) {
+			result2 = NAN;
+		}
+		printf("| %4.2f |       | %6.4f |            | %6.4f |\n", x, result1, result2);
 	}
+	printf("______________________________________________________\n");
 	return 0;
 }
 
@@ -42,8 +51,9 @@ double series(double x, double epsilon)
 	double result = 0.0;
 	double term = 1.0;
 	int k = 0;
+	int maxIterations = 10000;
 
-	while (fabs(term) > epsilon)
+	while (fabs(term) > epsilon && k < maxIterations) 
 	{
 		result += term;
 		k++;
